49e7ff5cc345f74e3266528a9e9c3ad5aabb8b03
CSE-17-dipshikha <dipshikhaarya157@gmail.com>
2026-05-31 21:10:40 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,36 +1,9 @@
-function changeText(){
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-    let msg =
-        document.getElementById("message").value;
-
-    document.getElementById("heading").innerHTML = msg;
-}
-
-function changeColor(){
-
-    document.body.style.backgroundColor = "lightblue";
-}
-
-function changeFont(){
-
-    document.getElementById("heading").style.fontSize =
-        "50px";
-}
-
-function togglePara(){
-
-    let para = document.getElementById("para");
-
-    if(para.style.display == "none"){
-        para.style.display = "block";
-    }
-
-    else{
-        para.style.display = "none";
-    }
-}
-
-function resetPage(){
-
-    location.reload();
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
